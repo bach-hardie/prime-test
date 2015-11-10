a59a2a25cb877b44f05755511d0acf8193bdf820
J Bach Hardie <josebh@ebi.ac.uk>
2015-11-10 15:21:10 +0000

    First draft of the prime sieve function

---
+++ coffee/prime-multiply.coffee
@@ -0,0 +1,27 @@
+getPrimes = (n) ->
+  # First off some input checking
+  if n is parseInt(n)
+    throw new TypeError "Input not an integer"
+  if n < 1
+    throw new RangeError "Input not a positive integer"
+
+  # The two special cases of n=1 and n=2
+  if n == 1
+    return []
+  if n == 2
+    return [2]
+
+  array = (true for x in [0..n]) # The list of candidates
+  sqrt_limit = Math.sqrt(n + 1) # We need to only calculate primes up to this value
+
+  # Implementation of the Sieve of Eratosthenes. Will discard non-primes in sequence
+  for i in [2..sqrt_limit]
+    do (i) ->
+      if array[i]
+        for j in [(i*i)..n] by i
+          do (j) ->
+            array[j] = false
+
+  # Get all keys not sieved out.
+  primes = (p for p in [0..(n - 1)] when array[p])
+  return primes
--- js/prime-multiply.js
+++ js/prime-multiply.js
@@ -1,1 +1,52 @@
+var getPrimes;
 
+getPrimes = function(n) {
+  var array, fn, i, k, p, primes, ref, sqrt_limit, x;
+  if (n === parseInt(n)) {
+    throw new TypeError("Input not an integer");
+  }
+  if (n < 1) {
+    throw new RangeError("Input not a positive integer");
+  }
+  if (n === 1) {
+    return [];
+  }
+  if (n === 2) {
+    return [2];
+  }
+  array = (function() {
+    var k, ref, results;
+    results = [];
+    for (x = k = 0, ref = n; 0 <= ref ? k <= ref : k >= ref; x = 0 <= ref ? ++k : --k) {
+      results.push(true);
+    }
+    return results;
+  })();
+  sqrt_limit = Math.sqrt(n + 1);
+  fn = function(i) {
+    var j, l, ref1, ref2, ref3, results;
+    if (array[i]) {
+      results = [];
+      for (j = l = ref1 = i * i, ref2 = n, ref3 = i; ref3 > 0 ? l <= ref2 : l >= ref2; j = l += ref3) {
+        results.push((function(j) {
+          return array[j] = false;
+        })(j));
+      }
+      return results;
+    }
+  };
+  for (i = k = 2, ref = sqrt_limit; 2 <= ref ? k <= ref : k >= ref; i = 2 <= ref ? ++k : --k) {
+    fn(i);
+  }
+  primes = (function() {
+    var l, ref1, results;
+    results = [];
+    for (p = l = 0, ref1 = n - 1; 0 <= ref1 ? l <= ref1 : l >= ref1; p = 0 <= ref1 ? ++l : --l) {
+      if (array[p]) {
+        results.push(p);
+      }
+    }
+    return results;
+  })();
+  return primes;
+};
